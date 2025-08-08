40f5732ca6260581c977bbac966b45e7da5f07f7
hiverkiya <hiverkiya@gmail.com>
2025-08-08 16:00:09 +0530

    modifying prisma schema

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,40 @@
+"use client";
 import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { useTRPC } from "@/trpc/client";
+import { useMutation } from "@tanstack/react-query";
+import { useState } from "react";
+import { toast } from "sonner";
 
 const HomePage = () => {
+  const trpc = useTRPC();
+  const invoke = useMutation(
+    trpc.invoke.mutationOptions({
+      onSuccess: () => {
+        toast.success("Build started");
+      },
+    })
+  );
+
+  const [value, setValue] = useState("");
   return (
-    <div>
-      <Button>Click on me to generate something</Button>
+    <div
+      className="flex
+     items-center justify-center
+     "
+    >
+      <Input
+        placeholder="Enter something to generate"
+        className="max-w-3xl border-black p-6 m-4"
+        value={value}
+        onChange={(e) => setValue(e.target.value)}
+      />
+      <Button
+        disabled={invoke.isPending}
+        onClick={() => invoke.mutate({ text: "John" })}
+      >
+        Click on me to generate something
+      </Button>
     </div>
   );
 };
--- src/trpc/routers/_app.ts
+++ src/trpc/routers/_app.ts
@@ -1,17 +1,22 @@
 import { z } from "zod";
 import { baseProcedure, createTRPCRouter } from "../init";
+import { inngest } from "@/inngest/client";
 
 export const appRouter = createTRPCRouter({
-  hello: baseProcedure
+  invoke: baseProcedure
     .input(
       z.object({
         text: z.string(),
       })
-    )
-    .query((opts) => {
-      return {
-        greeting: `hello ${opts.input.text}`,
-      };
-    }),
-});
-export type AppRouter = typeof appRouter;
+    ).mutation(async({input})=>{
+      await inngest.send({
+        name:"test/hello.world",
+        data:{
+          email:input.text
+        }
+      })
+    }
+  ),
+  createAI:baseProcedure.input(
+    
+  )
